123ad3bebc51ca2824771033e6ef9e6c5476dbe2
masudaS <masuda@infocity.co.jp>
2021-12-21 13:30:48 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ btn.onclick = () => {
   const rect = document.getElementById("rect") as any
 
   console.log(typeof rect.requestFullscreen)
-  if (typeof rect.requestFullscreen) {
+  if (typeof rect.requestFullscreen === "function") {
     rect.requestFullscreen()
   }
 
   console.log(typeof rect.webkitRequestFullscreen)
-  if (typeof rect.webkitRequestFullscreen) {
+  if (typeof rect.webkitRequestFullscreen === "function") {
     rect.webkitRequestFullscreen()
   }
 }
